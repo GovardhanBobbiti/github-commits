484b31c9e1342041b7ae94a417bbb92a96cbb440
Govardhan Reddy <govardb@altair.com>
2022-10-29 09:24:05 +0530

    total functionality done

--- src/components/commits.js
+++ src/components/commits.js
@@ -2,42 +2,78 @@ import React, { useState, useEffect } from 'react';
 import { Octokit } from "@octokit/core";
 import Commit from './commit';
 
+const owner = "GovardhanBobbiti",
+      repo = "github-commits";
+
+let timer;
+let COUNTDOWN_TIME = 30;
+
 const CommitsPage = ({ accessToken }) => {
     const [commits, setCommits] = useState([]);
-   
-
-    useEffect(() => {
+    const [countdown, setCountDown] = useState(0);
+    const [isTimerOn, setTimerOn] = useState(false);
+ 
+    const fetchCommits = async () => {
         const octokit = new Octokit({
             auth: accessToken
         });
+        const fiveMostRecentCommits = await octokit.request(
+            `GET /repos/{owner}/{repo}/commits`,
+            { owner, repo }
+        );
+        setTimerOn(true);
+        setCommits(fiveMostRecentCommits.data);
+    }
 
-      const owner = "GovardhanBobbiti",
-      repo = "github-commits";
-
-        async function fetchCommits() {
-            const fiveMostRecentCommits = await octokit.request(
-                `GET /repos/{owner}/{repo}/commits`,
-                { owner, repo }
-            );
-            setCommits(fiveMostRecentCommits.data);
+    useEffect(() => {
+        if(isTimerOn) {
+            timer  = setInterval(() => {
+                setCountDown(state => (state - 1))
+            }, 1000); 
         }
-        fetchCommits();
+    }, [isTimerOn]);
+   
+    useEffect(() => {
+        if(countdown === -1) {
+            refetchCommits();  
+        }
+    }, [countdown]);
 
-    }, [accessToken]);
+    
+
+    useEffect(() => {
+        setCountDown(COUNTDOWN_TIME);
+        fetchCommits();
+    }, []);
+
+    const refetchCommits = () => {
+        setCommits([]);
+        setCountDown(COUNTDOWN_TIME);
+        setTimeout(() => {
+            fetchCommits();
+        }, 500);     
+    }
 
     return (
         <div>
-            <h1>Commits</h1>
+            <div className='commits-header'>
+                <h1>Commits</h1>
+                <div>
+                    <button onClick={refetchCommits}>Refresh</button>
+                    <span>{countdown}</span>
+                </div>
+            </div>
+            
             <div className='commits-container'>
                 {
-                    commits ? commits.map(({commit}, index) => {
+                    commits.length > 0 ? commits.map(({commit}, index) => {
                         return <Commit 
                                     key={index} 
                                     message={commit.message} 
                                     committer={commit.committer.name} 
                                     date={commit.committer.date}
                                 />
-                    }) : null
+                    }) : (<div>Loading...</div>)
                 }
             </div>
         </div>
